00d35051dc8512dd74ccf4baebb484dd7e396e55
Gilad Tal <122132000+Greenjolteon@users.noreply.github.com>
2023-09-18 22:55:25 -0400

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,250 +1,217 @@
-/*
-To-do:
-*/
-
 function numapp(item) {
-	let input = document.form.input;
-	input.value += item;
+	document.form.input.value += item;
 }
 
 function reset() {
-	let input = document.form.input;
-	input.value = "";
-	console.clear();
+	let input = document.form.input.value = "";
 }
 
-document.addEventListener('keydown', function(event) {
-	if (event.key === 'Enter') {
-		equate()
-	}
-});
+document.addEventListener("keydown", (function(e) {
+	"Enter" === e.key && equate()
+}));
 
 function equate() {
 	let input = document.form.input;
-	let output = document.form.output;
-	let inputValue = input.value;
-	let array = [];
-	let paroi = 1;
-	//separate the numbers and operators
-	for (let i = 0; i < inputValue.length; i++) {
-		if (isDigit(inputValue[i])) {
-			if (isDigit(inputValue[i - 1]) || inputValue[i - 1] === '.') {
-				array[array.length - 1] = array[array.length - 1].concat(inputValue[i]);
-			} else {
-				array.push(inputValue[i]);
-			}
-		} 
-		else if (inputValue[i] === 'e') {
-		  array.push((Math.E).toString());
-		}
-		else if (inputValue[i] === 'π') {
-		  array.push((Math.PI).toString());
-		}
-		else if (inputValue[i] === 'l') { // i = 3
-		  if (inputValue[i + 1] === 'o' && inputValue[i + 2] === 'g') {
-		      if (isDigit(inputValue[i + 3])) {
-		      array[i] = 'l' + 'o' + 'g';
-		      array[i] = array[i].concat(inputValue[i+3]);
-		      i+=3;
-		      }
-		      else {
-		      funcs(array, 'l', 'o', 'g', i);
-		      i += 2;
-		      }
-		    }
-		  else if (inputValue[i + 1] === 'n') {
-		    array[i] = 'l';
-		    array[i+1] = 'n';
-		    array[i] = array[i].concat(array[i+1]);
-		    array.splice(i+1, 1);
-		    i++;
-		  }
-		  paroi = 0;
-		}
-		else if (inputValue[i] === 's' && inputValue[i + 1] === 'i' && inputValue[i + 2] === 'n') {
-		      funcs(array, 's', 'i', 'n', i);
-		      i += 2;
-	        paroi = 0;
-		}
-				else if (inputValue[i] === 'c' && inputValue[i + 1] === 'o' && inputValue[i + 2] === 's') {
-		      funcs(array, 'c', 'o', 's', i);
-		      i += 2;
-		      paroi = 0;
-		}
-				else if (inputValue[i] === 't' && inputValue[i + 1] === 'a' && inputValue[i + 2] === 'n') {
-		      funcs(array, 't', 'a', 'n', i);
-		      i += 2;
-		      paroi = 0;
-		}
-		else {
-			array.push(inputValue[i]);
-		}
+	let out = document.form.output;
+	let array = input.value.match(/([0-9.]+|[+\-*/()√!]|log|ln|sin|cos|tan|sinh|cosh|tanh|π|e)/g);
 
-		if ((inputValue[i + 1] === '(') && (paroi === 1)) {
-			array.push('*');
-		}
-	  } 
-	
-	console.log("Beginning 1: " + array);
-	//connecting numbers with decimals
-	for (let i = 0; i < array.length; i++) {
-	  if (array[i].includes('.') && array[i] !== (Math.E).toString() && array[i] !== (Math.PI).toString()) {
-	    array[i - 1] = array[i - 1].concat(array[i]);
-	    array.splice(i, 1);
-	  }
+	for (let l=0; l<array.length; l++) {
+		if ((constCheck(array[l]) !== l) && isDigit(array[l - 1])) {array.splice(l, 0, '*');}
 	}
-	
-	console.log("Beginning 2: " + array);
-	// P
-	let psFound = 0;
-	let pfFound = 0;
-	let psPos = -1;
-	let pfPos = -1;
+	console.log("Array: " + array);
+
+	const precedence = {'^': 4,  '√': 3, 'sin': 3, 'cos': 3, 'tan': 3, 'sinh': 3, 'cosh': 3, 'tanh': 3, 'log': 3, 'ln': 3, '!': 3, '*': 2, '/': 2, '+': 1, '-': 1,};
+	//put all functions not in emdas between ^ and *
+
+	const associative = {'^': 0,  '√': 1, 'sin': 1, 'cos': 1, 'tan': 1, 'sinh': 1, 'cosh': 1, 'tanh': 1, 'log': 1, 'ln': 1, '!': 1, '*': 1, '/': 1, '+': 1, '-': 1,};
+
+	//change from normal equation to RPN
+
+	let output = [];
+	let stack = [];
+
 	for (let j = 0; j < array.length; j++) {
-		if (array[j] === '(') {
-			psPos = j + 1;
-			psFound = 1;
+		let x = array[j];
+		if (isDigit(x)) {
+			output.push(constCheck(x));
+		} else if (x == '(') {
+			stack.push(x);
+		} else if (isOperator(x)) {
+			while ((isOperator(stack[stack.length - 1])) && (stack.length > 0) &&
+				(associative[x] === 1 && precedence[x] <= precedence[stack[stack.length - 1]]) ||
+				(associative[x] === 0 && precedence[x] < precedence[stack[stack.length - 1]])) {
+				output.push(stack.pop());
+			}
+			stack.push(x);
+		} else if (x == ')') {
+			while (stack[stack.length - 1] !== '(') {
+				output.push(stack.pop());
+			}
+			stack.pop();
 		}
-		if (array[j] === ')') {
-			pfPos = j - 1;
-			pfFound = 1;
-		}
+	}
+	while (stack.length > 0) {
+		output.push(stack.pop());
 	}
 
-	if (psFound && pfFound) {
-		let numToRemove = pfPos - psPos + 3;
-		let nArray = array.slice(psPos, pfPos+1)
-		let emdasResult = emdas(nArray, pfPos);
-		array.splice(psPos - 1, numToRemove, ...emdasResult);
-		console.log("After P splice: " + array);
+	console.log("Final Output in RPN: " + output);
+
+	//Now, solve using the RPN
+
+	for (let q = 0; q < output.length; q++) {
+		let y = output[q];
+		if (isOperator(y)) {
+			let num1 = stack.pop();
+			if (y === '√') {
+				stack.push(Math.sqrt(num1));
+			} else if (y === 'sin') {
+				stack.push(Math.sin(num1));
+			} else if (y === 'cos') {
+				stack.push(Math.cos(num1));
+			} else if (y === 'tan') {
+				stack.push(Math.tan(num1));
+			} else if (y == 'log') {
+				stack.push(Math.log10(num1));
+			} else if (y == 'ln') {
+				stack.push(Math.log(num1));
+			} else if (y == 'sinh') {
+				stack.push(Math.sinh(num1));
+			} else if (y == 'cosh') {
+				stack.push(Math.cosh(num1));
+			} else if (y == 'tanh') {
+				stack.push(Math.tanh(num1));
+			} else if (y == '!') {
+				stack.push(factorial(num1));
+			} else {
+				const num2 = stack.pop();
+				switch (y) {
+					case '+':
+						stack.push(num2 + num1);
+						break;
+					case '-':
+						stack.push(num2 - num1);
+						break;
+					case '*':
+						stack.push(num2 * num1);
+						break;
+					case '/':
+						stack.push(num2 / num1);
+						break;
+					case '^':
+						stack.push(num2 ** num1);
+						break;
+				}
+			}
+		} else {
+			stack.push(parseFloat(y));
+		}
+		console.log("token: " + y);
+		console.log("stack: [" + stack + "]");
 	}
-	
-	//solve other functions
-	for (let l = 0; l < array.length; l++) {
-    //solve natural logs
-    if (array[l] === 'ln') {
-      array[l] = Math.log(parseFloat(array[l + 1]));
-      array.splice(l + 1, 1);
-    }
-    //solve other logs
-    else if (array[l].includes('log')) {
-      if (array[l] === 'log') {
-        array[l] = Math.log10(parseFloat(array[l + 1]));
-        array.splice(l + 1, 1);
-      }
-      else {
-        let base = array[l].slice(3);
-        console.log(base, parseFloat(array[l+1]));
-        array[l] = ((Math.log(parseFloat(array[l + 1]))) / (Math.log(base)));
-        array.splice(l + 1, 1);
-      }
-    }
-    //solve sin
-    else if (array[l] === 'sin') {
-        array[l] = Math.sin(parseFloat(array[l + 1]));
-        array.splice(l + 1, 1);
-    }
-        else if (array[l] === 'cos') {
-        array[l] = Math.cos(parseFloat(array[l + 1]));
-        array.splice(l + 1, 1);
-    }
-        else if (array[l] === 'tan') {
-        array[l] = (Math.sin(parseFloat(array[l + 1]))/Math.cos(parseFloat(array[l + 1])));
-        array.splice(l + 1, 1);
-    }
+
+	floatnumCorrect(stack);
+
+	if (stack.length > 1) {
+		out.value = "Error";
+	} else {
+		out.value = stack[0];
+	}
+}
+
+function isDigit(num) {
+	return (!isNaN(constCheck(num)) && isFinite(constCheck(num)))
+}
+
+function isOperator(op) {
+	return (
+		op == '+' ||
+		op == '-' ||
+		op == '*' ||
+		op == '/' ||
+		op == '^' ||
+		op == '√' ||
+		op == 'sin' ||
+		op == 'cos' ||
+		op == 'tan' ||
+		op == 'sinh' ||
+		op == 'cosh' ||
+		op == 'tanh' ||
+		op == 'log' ||
+		op == 'ln' ||
+		op == '!'
+	)
+}
+
+function constCheck(t) {
+	if (t == 'e') {
+		return Math.E;
+	} else if (t == 'π') {
+		return Math.PI;
+	}
+	return t;
+}
+
+function concatenate(array, thing, times) {
+	for (let c = 0; c < times; c++) {
+		array[thing - 1] = array[thing - 1].concat(array[thing]);
+		array.splice(thing, 1);
+	}
+}
+
+function factorial(num) {
+	if (num === 0 || num === 1)
+	  return 1;
+	for (var i = num - 1; i >= 1; i--) {
+	  num *= i;
+	}
+	return num;
   }
-	console.log("After function solves: " + array);
 
-	// EMDAS
-	array = emdas(array, array.length);
+function floatnumCorrect(array) {
+	//constant check time
+	if (Math.abs(array[0] - Math.E) < (10 ** -10)) {
+		array[0] = 'e ≈ 2.718281828459045';
+	} else if (Math.abs(array[0] - Math.PI) < (10 ** -10)) {
+		array[0] = 'π ≈ 3.141592653589793';
+	} else if (Math.abs(array[0] - Math.SQRT2) < (10 ** -10)) {
+		array[0] = '√2 ≈ 1.4142135623730951';
+	} else if (Math.abs(array[0] - Math.SQRT1_2) < (10 ** -10)) {
+		array[0] = '√2/2 ≈ 0.7071067811865476';
+	} else if (Math.abs(array[0] - Math.LN2) < (10 ** -10)) {
+		array[0] = 'ln(2) ≈ 0.6931471805599453';
+	} else if (Math.abs(array[0] - Math.LN10) < (10 ** -10)) {
+		array[0] = 'ln(10) ≈ 2.302585092994046';
+	} else if (Math.abs(array[0] - Math.LOG2E) < (10 ** -10)) {
+		array[0] = 'log₂(e) ≈ 1.4426950408889634';
+	} else if (Math.abs(array[0] - Math.LOG10E) < (10 ** -10)) {
+		array[0] = 'log(e) ≈ 0.4342944819032518';
+	} else if (Math.abs(array[0] - Math.sqrt(3)) < (10 ** -10)) {
+		array[0] = '√3 ≈ 1.7320508075688772';
+	} else if (Math.abs(array[0] - (Math.sqrt(3) / 2)) < (10 ** -10)) {
+		array[0] = '√3/2 ≈ 0.8660254037844386';
+	}
+	//other constants can be put here
+	else {
+		for (let q = 2; q < 500; q++) {
+			if (Math.abs(array[0] - (1 / q)) < (10 ** -10)) {
+				array[0] = '1/' + q + ' ≈ ' + 1 / q;
+				return 0;
+			}
+			for (let p = 2; p < q; p++) {
+				if (Math.abs(parseFloat(array[0]) - (p / q)) < (10 ** -10)) {
+					array[0] = p + '/' + q + ' ≈ ' + p / q;
+					return 0;
+				}
+			}
+		}
+	}
 	if (array.length !== 1) { //error unless there is only one thing in the array
-	  array[0] = "Error"
+		array[0] = "Error"
+	} else if (Math.abs(parseFloat(array[0])) > (10 ** 10)) { //if the answer is more than 10^10 away from 0, it is set to Infinity
+		array[0] = ("Infinity").toString();
+	} else if (Math.abs((Math.ceil(Math.abs(parseFloat(array[0])))) - (Math.abs(parseFloat(array[0])))) < (10 ** -10)) { //if the answer is less than 10^10 lower than an int, it is rounded up
+		array[0] = Math.ceil(parseFloat(array[0]));
+	} else if (Math.abs((Math.floor(Math.abs(parseFloat(array[0])))) - (Math.abs(parseFloat(array[0])))) < (10 ** -10)) { //if the answer is less than 10^10 higher than an int, it is rounded down
+		array[0] = Math.floor(parseFloat(array[0]));
 	}
-	else if (Math.abs(parseFloat(array[0])) < (10 ** -10)){ //if the answer is more than 10^-10 away from 0, it is set to 0
-    array[0] = ('0').toString();
-	}
-	else if (Math.abs(parseFloat(array[0])) > (10 ** 10)){ //if the answer is more than 10^10 away from 0, it is set to Infinity
-    array[0] = ("Infinity").toString();
-	}
-	console.log(array[0]);
-	output.value = array[0];
-}
-
-function emdas(earray, endPosition) {
-	//E
-	console.log("First earray: " + earray);
-	for (let j = 0; j <= endPosition; j++) {
-		if ((earray.length === 1) || !(earray.includes('^'))) {
-		  break;
-		}
-		else if (earray[j] === '^') {
-			let num1 = parseFloat(earray[j - 1]);
-			let num2 = parseFloat(earray[j + 1]);
-			earray[j - 1] = (Math.pow(num1, num2)).toString();
-			earray.splice(j, 2);
-		}
-		console.log("E run #" + j + ": " + earray);
-	}
-	console.log("After E earray: " + earray);
-	//MD
-	for (let j = 0; j <= endPosition; j++) {
-		if ((earray.length === 1) || (!(earray.includes('*')) && !(earray.includes('/')))) {
-		  break;
-		}
-		else if ((earray[j] === '*') || (earray[j] === '/')) {
-			let num1 = Number(earray[j - 1]);
-			let num2 = Number(earray[j + 1]);
-			if (earray[j] === '*') {
-				earray[j - 1] = (num1 * num2).toString();
-			}
-			if (earray[j] === '/') {
-				earray[j - 1] = (num1 / num2).toString();
-			}
-			earray.splice(j, 2);
-			j--;
-		}
-		console.log("MD run #" + j + ": " + earray);
-	}
-	console.log("After MD earray: " + earray);
-	//AS
-	for (let j = 0; j <= endPosition; j++) {
-		if ((earray.length === 1) || (!(earray.includes('+')) && !(earray.includes('-')))) {
-		  break;
-		}
-		else if ((earray[j] === '+') || (earray[j] === '-')) {
-			let num1 = Number(earray[j - 1]);
-			let num2 = Number(earray[j + 1]);
-			if (earray[j] === '+') {
-				earray[j - 1] = (num1 + num2).toString();
-			}
-			if (earray[j] === '-') {
-				earray[j - 1] = (num1 - num2).toString();
-			}
-			earray.splice(j, 2);
-			j--;
-		}
-		console.log("AS run #" + j + ": " + earray);
-	}
-	console.log("After AS earray: " + earray);
-	return earray;
-}
-
-function isDigit(character) {
-	return ((Number(character) >= 0) && (Number(character) <= 9))
-}
-
-function isAlpha(str) {
-  for (let i = 0; i < str.length; i++) {
-    const charCode = str.charCodeAt(i);
-    return (!(charCode >= 65 && charCode <= 90) && !(charCode >= 97 && charCode <= 122))
-  }
-}
-
-function funcs(farray, a, b, c, i) {
-  farray[i] = a;
-	farray[i+1] = b;
-	farray[i+2] = c;
-	farray[i] = farray[i].concat(farray[i+1]);
-	farray[i] = farray[i].concat(farray[i+2]);
-	farray.splice(i+1, 2);
 }
